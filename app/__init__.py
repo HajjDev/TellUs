96355d886d022b0a948516573b15166331a8bdc6
Charbel El Hajj <charbel.elhajj@student.uclouvain.be>
2025-03-20 16:31:59 +0100

    Update __init__.py: Recommiting changes for DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,22 @@
 from flask import Flask, render_template
-import os
 from .blueprints import login
-from . import database
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+ 
+app = Flask(__name__, instance_relative_config=True) 
+app.config.from_mapping(
+    SECRET_KEY = '2@CJIZD9081',
+)
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY = '2@CJIZD9081',
-        DATABASE = os.path.join(app.instance_path, 'flights.sqlite')
-    )
+basedir = os.path.abspath(os.path.join(os.getcwd(), "app"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'tellus.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-    if test_config is None:
-        # Charge l'instant, si elle n'existe pas, ne charge pas
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # Charge si elle existe
-        app.config.from_mapping(test_config)
+migrate = Migrate(app, db) 
+app.register_blueprint(login.bp)
 
-    # Regarde si le fichier existe bien
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # Charge la base de données
-    database.innit_app(app)
-    
-    app.register_blueprint(login.bp)
-    
-    @app.route('/')
-    def signupin():
-        return render_template('index.html')
-
-    return app
+@app.route('/') 
+def signupin():
+    return render_template('index.html')
