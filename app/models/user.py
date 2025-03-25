58ced27c8c423470626155ef038ccdcd0faaf61d
Asaph Nzongan <asaph.nzongan@student.uclouvain.be>
2025-03-25 11:17:10 +0100

    database fixing

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,11 @@
-from app.__init__ import db
+from app import db
 from datetime import datetime
 
 class User(db.Models):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(64), index=True, unique=True)
-    lastname = db.Column(db.String(64), index=True)
-    username = db.Column(db.String(64), index=True)
+    lastname = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     phone = db.Column(db.Integer, index=True, unique=True)
     password_hash = db.Column(db.String(128))
--- app/sign_up.py
+++ app/sign_up.py
@@ -1,7 +1,7 @@
 from flask_wtf import Flaskform
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
-from app.models  import User
+from app.models.user  import User
 
 class RegistrationForm(FlaskForm) :
     firstname = StringField('firstname', validator=[DataRequired()])
