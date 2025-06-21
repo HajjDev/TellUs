2434c2833c16e33d0b980ba17d6ac8407dd5e0dc
Asaph Nzongan <asaph.nzongan@student.uclouvain.be>
2025-06-21 16:52:02 +0200

    adding packages in package.json

--- backend/route/auth.js
+++ backend/route/auth.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
 const router = express.Router();
+
+router.use(express.json());
+router.use(morgan('dev'));
+
+router.post('/login', (req, res)=>{
+    
+});
