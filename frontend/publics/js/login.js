2c951c60ee208cae3744419f3bc51ceb7229597b
Asaph Nzongan <asaph.nzongan@student.uclouvain.be>
2025-06-21 16:40:46 +0200

    adding login form and login.js handler

---
+++ frontend/publics/js/login.js
@@ -0,0 +1,12 @@
+document.getElementById("login-button").addEventListener('click', async ()=>{
+    try{
+
+        const url = "";
+        const ename = document.getElementById('ename').value;
+        const password = document.getElementById('password').value;
+        response = await fetch()
+
+    }catch(error){
+        console.error(error.message);
+    }
+})
