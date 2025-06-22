36a3280af5b306471e5d248e14e383e1594df323
Asaph Nzongan <asaph.nzongan@student.uclouvain.be>
2025-06-22 18:34:31 +0200

    improving login request in login.js

--- frontend/publics/js/login.js
+++ frontend/publics/js/login.js
@@ -1,23 +1,29 @@
 document.getElementById("login-button").addEventListener('click', async ()=>{
     try{
 
-        const url = "";
+        const url = "http://localhost:3001/api/auth/login";
         const ename = document.getElementById('ename').value;
         const password = document.getElementById('password').value;
-        response = await fetch()
+        const response = await fetch(url, {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+               ename:ename,
+               password:password 
+            })
+        })
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = response.json();
+        console.log(json);
 
     }catch(error){
         console.error(error.message);
     }
 })
-
-const url ; //mongo_URI
-
-fetch(url,{
-    method:"GET",
-    headers:{
-        
-    }
-}
-
-).then(()=>{console.log()})
