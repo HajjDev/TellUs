9ec1684148f58402f2eafc2e8dc663203f22fba9
Charbel El Hajj <charbel.elhajj@student.uclouvain.be>
2025-07-20 12:41:22 +0200

    Restructuring and deleting old files

--- frontendOld/publics/js/enable_mfa.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("enable_totp").addEventListener("click", ()=>{
-    window.location.href = "../html/enable_totp.html";
-});
-
-document.getElementById("enable_otp").addEventListener("click", ()=>{
-    window.location.href = "../html/enable_otp.html";
-});
